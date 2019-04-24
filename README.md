8c4f5269b309c9468b4bbc430a55af2cf57293bb
Your Name <hp2393@gmail.com>
2019-04-23 21:37:32 -0400

    updated liri

--- README.md
+++ README.md
@@ -1,1 +1,62 @@
-# liri-node-app
+# LIRI-Bot
+LIRI is like iPhone's SIRI. However, while SIRI is a Speech Interpretation and Recognition Interface, LIRI is a_Language_ Interpretation and Recognition Interface. LIRI will be a command line node app that takes in parameters and gives you back data.
+
+## LIRI uses the following commands:
+```
+my-tweets, spotify-this-song, movie-this, do-what-it-says
+```
+
+## Technologies used:
+
+1. Node.js
+2. Javascript
+
+## npm packages: 
+
+1. [spotify](https://www.npmjs.com/package/node-spotify-api) - A simple to use API library for the Spotify REST API.
+2. [request](https://www.npmjs.com/package/request) - Request is designed to be the simplest way possible to make http calls. It supports HTTPS and follows redirects by default.
+3. [dotenv](https://www.npmjs.com/package/dotenv) - Dotenv is a zero-dependency module that loads environment variables from a .env file into process.env.
+
+## How to Run LIRI-Bot
+
+* Step One: node liri spotify-this-song ```<song name here>```.
+	
+	This will show the following information about the song in your terminal/bash window: 
+	* Artist(s) 
+	* The song's name 
+	* A preview link of the song from Spotify 
+	* The album that the song is from
+
+	If no song is provided then the program will default to
+	"My Heart Will Go On" by Celine Dion
+* Step Two: node liri.js movie-this ```<movie name here>```.
+	
+	This will output the following information to your terminal/bash window:
+	* Title of the movie.
+	* Year the movie was released.
+	* IMDB Rating of the movie.
+	* Country where the movie was produced.
+	* Language of the movie.
+	* Plot of the movie.
+	* Actors in the movie.
+	* Rotten Tomatoes Rating.
+	* Rotten Tomatoes URL.
+	
+	If the user doesn't type a movie in, the program will output data for the movie 'Mr. Nobody'
+
+* Step Three: node liri.js do-what-it-says
+
+	This will output the command placed in random.txt file
+
+## Problems faced:
+
+1. Had hard time understanding the dot in front of files that we dont want to be tracked such as `.env`
+2. Forgot to install certain npm packages before running the code
+3. I requested API key for Bandsintown yesterday. They had asked me to provide details about why I needed the API key. They asked me to send what school I attend, teachers full name and email address inorder to get the API key. I provided that information last night but I still have not gotten anything back. I was not able to work on command `concert-this`
+
+
+
+## Author
+ **Harsh Patel** 
+
+
--- liri.js
+++ liri.js
@@ -1,1 +1,120 @@
 require("dotenv").config();
+
+//variables
+var keys = require("./keys.js");
+var fs = require("fs");
+var request = require("request");
+//var Spotify = require('spotify-web-api-node');
+var Spotify = require('node-spotify-api');
+
+//argv[2] chooses users actions; argv[3] is input parameter, ie; movie title
+var userCommand = process.argv[2];
+var secondCommand = process.argv[3];
+
+//concatenate multiple words in 2nd user argument
+for (var i = 4; i < process.argv.length; i++) {
+    secondCommand += '+' + process.argv[i];
+}
+
+// Getting Spotify Keys
+var spotify = new Spotify(keys.spotify);
+// Function for running a Spotify search
+var getSpotify = function () {
+    if (!secondCommand) {
+        secondCommand = "I Want it That Way";
+    }
+    console.log("getting info" + secondCommand);
+
+    spotify.search(
+        {
+            type: "track",
+            query: secondCommand
+        },
+        function (err, data) {
+            if (err) {
+                console.log("Error occurred: " + err);
+                return;
+            }
+
+            var songs = data.tracks.items;
+
+            for (var i = 0; i < songs.length; i++) {
+                console.log(i);
+                console.log("artist(s): " + songs[i].artists[0].name);
+                console.log("song name: " + songs[i].name);
+                console.log("preview song: " + songs[i].preview_url);
+                console.log("album: " + songs[i].album.name);
+                console.log("-----------------------------------");
+            }
+        }
+    );
+};
+
+//Switch command
+function mySwitch(userCommand) {
+    //choose which statement (userCommand) to switch to and execute
+    switch (userCommand) {
+        case "spotify-this-song":
+        getSpotify();
+        break;
+
+        case "do-what-it-says":
+        doWhat();
+        break;
+
+        case "movie-this":
+        getMovie();
+        break;
+    }
+
+    //OMDB Movie: movie-this
+    function getMovie() {
+        // OMDB Movie - this MOVIE base code is from class files, I have modified for more data and assigned parse.body to a Var
+        var movieName = secondCommand;
+        // Then run a request to the OMDB API with the movie specified
+        var queryUrl = "http://www.omdbapi.com/?t=" + movieName + "&apikey=8d78771";
+        console.log(queryUrl)
+
+        request(queryUrl, function (error, response, body) {
+
+            // If the request is successful = 200
+            if (!error && response.statusCode === 200) {
+                var body = JSON.parse(body);
+                console.log('================ Movie Info ================');
+                console.log("Title: " + body.Title);
+                console.log("Release Year: " + body.Year);
+                console.log("IMdB Rating: " + body.imdbRating);
+                console.log("Country: " + body.Country);
+                console.log("Language: " + body.Language);
+                console.log("Plot: " + body.Plot);
+                console.log("Actors: " + body.Actors);
+                console.log("Rotten Tomatoes Rating: " + body.Ratings[2].Value);
+                console.log("Rotten Tomatoes URL: " + body.tomatoURL);
+                console.log('==================THE END=================');
+            } else {
+                //else - throw error
+                console.log("Error occurred.")
+            }
+            //Response if user does not type in a movie title
+            if (movieName === "Mr. Nobody") {
+                console.log("-----------------------");
+                console.log("If you haven't watched 'Mr. Nobody,' then you should: http://www.imdb.com/title/tt0485947/");
+                console.log("It's on Netflix!");
+            }
+        });
+    }
+
+    //Function for command do-what-it-says
+    function doWhat() {
+        //Read random.txt file
+        fs.readFile("random.txt", "utf8", function (error, data) {
+            if (!error);
+            console.log(data.toString());
+
+            //split text with comma 
+            var split = data.toString().split(',');
+        });
+    }
+}
+//Call mySwitch function
+mySwitch(userCommand);
